344102bda7789f0bbba7c32b011ba7fc552dedd8
Loli Slayer <54113717+loli-slayer@users.noreply.github.com>
2021-03-30 22:01:13 +0700

    Update DiskTest.java

--- Lab05/DiskTest.java
+++ Lab05/DiskTest.java
@@ -2,9 +2,9 @@ public class DiskTest {
     public static void main(String[] args) {
         Orders o = new Orders();
         DigitalVideoDisc dvd4 = new DigitalVideoDisc(14, 7.22f,"Ngoc","LOL", "Comedy");
-        DigitalVideoDisc dvd5 = new DigitalVideoDisc(99, 3.23f, "ABC","harry11","Horror");
-        DigitalVideoDisc dvd6 = new DigitalVideoDisc(11, 11.11f,"aae", "12 potter","Legend");
-        DigitalVideoDisc dvd7 = new DigitalVideoDisc(13, 12.22f, "wtf", "aef harry potter", "aeea");
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc(99, 3.23f, "ABC","HaRry11","Horror");
+        DigitalVideoDisc dvd6 = new DigitalVideoDisc(11, 11.11f,"aae", "12 potTer","Legend");
+        DigitalVideoDisc dvd7 = new DigitalVideoDisc(13, 12.22f, "wtf", "aef Harry potter", "aeea");
         DigitalVideoDisc dvd8 = new DigitalVideoDisc(13, 11.33f, "aawfw", "faw", "fwafwa");
         DigitalVideoDisc dvd9 = new DigitalVideoDisc(44, 44.1f, "faae", "ewawa", "category");
         DigitalVideoDisc dvd10 = new DigitalVideoDisc(21, 21.21f,"aera","Aaf","araewrf");
